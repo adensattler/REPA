ad2b80a969e1fa39a50e3c37bc859786fceeb939
Jordan Sutherland <jordancsutherland@gmail.com>
2023-05-01 10:43:41 -0600

    User Story: Fixed now add all information from dummy table can start to be used for people to work off of

--- analytics/testAnalytics(zipcode).py
+++ analytics/testAnalytics(zipcode).py
@@ -4,8 +4,9 @@ import csv
 conn = sqlite3.connect('RealEstateProject.db')
 
 cur = conn.cursor()
+cur.execute("DROP TABLE IF EXISTS RealEstate")
 
-# Read the CSV file and get the header row
+# Read the CSV file and get the header row (make sure path to csv file is your own path on your local computer)
 with open('/Users/jordan/Desktop/Software Engineering/dataframe.csv') as csvfile:
     reader = csv.reader(csvfile)
     header = next(reader)
@@ -15,7 +16,7 @@ query = f"CREATE TABLE RealEstate ({', '.join(header)})"
 cur.execute(query)
 
 # Insert data into the table
-with open('path/to/data.csv') as csvfile:
+with open('/Users/jordan/Desktop/Software Engineering/dataframe.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         values = [row[column] for column in header]
