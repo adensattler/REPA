1eac2901443b13d6d483c5031e318e3a8bdd674e
Jordan Sutherland <jordancsutherland@gmail.com>
2023-05-01 10:37:39 -0600

    User Story: Makes table and database for sample data but doesn't fill it in correctly or at all

--- analytics/testAnalytics(zipcode).py
+++ analytics/testAnalytics(zipcode).py
@@ -1,15 +1,28 @@
 import sqlite3
+import csv
 
 conn = sqlite3.connect('RealEstateProject.db')
 
 cur = conn.cursor()
 
-cur.execute('''CREATE TABLE AreaInformation(
-                User_ID INTEGER PRIMARY KEY NOT NULL,
-                Product_ID INTEGER NOT NULL,
-                Name TEXT NOT NULL,
-                Gender TEXT NOT NULL,
-                AGE INTEGER NOT NULL,
-                CITY TEXT);
-                ''')
+# Read the CSV file and get the header row
+with open('/Users/jordan/Desktop/Software Engineering/dataframe.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    header = next(reader)
+
+# Create a new table with the header columns
+query = f"CREATE TABLE RealEstate ({', '.join(header)})"
+cur.execute(query)
+
+# Insert data into the table
+with open('path/to/data.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        values = [row[column] for column in header]
+        query = f"INSERT INTO RealEstate ({', '.join(header)}) VALUES ({', '.join(['?']*len(header))})"
+        cur.execute(query, values)
+
+# Commit changes and close the connection
 conn.commit()
+conn.close()
+
