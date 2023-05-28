f7830dc43a584bcec774e57cce9843e0640fac5a
Ibraheem Qureshi <ibraheem.qureshi@du.edu>
2023-05-27 18:00:42 -0600

    modified main to account for tax_ass_val field

--- Main/main.py
+++ Main/main.py
@@ -22,12 +22,13 @@ def main():
         # stores the columns we are interested in
         columns = [
             'zpid', 'hdpData.homeInfo.price', 'hdpData.homeInfo.bedrooms', 'hdpData.homeInfo.bathrooms', 'area',
-            'hdpData.homeInfo.zipcode', 'hdpData.homeInfo.livingArea', 'hdpData.homeInfo.homeType', 'hdpData.homeInfo.zestimate', 'hdpData.homeInfo.city', 'hdpData.homeInfo.latitude', 'hdpData.homeInfo.longitude'
+            'hdpData.homeInfo.zipcode', 'hdpData.homeInfo.livingArea', 'hdpData.homeInfo.homeType', 'hdpData.homeInfo.zestimate', 'hdpData.homeInfo.city', 'hdpData.homeInfo.latitude', 'hdpData.homeInfo.longitude', 
+            'hdpData.homeInfo.taxAssessedValue'	
         ]
 
         # Takes all of the data and converts it into normalized, tabular data (.json_normalize)
         den_listings = pd.json_normalize(listing_response["data"]["cat1"]["searchResults"]["mapResults"])
-        selected_den_listings = den_listings.loc[:, columns].dropna(thresh=12)
+        selected_den_listings = den_listings.loc[:, columns].dropna(thresh=13)
         create_database(selected_den_listings)
 
     if args.analyze:
