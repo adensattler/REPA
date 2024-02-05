d1b87b9a89e2ec2613dc331b049732dbcdd3f4bd
BasedWRLD <kevin2845ca@gmail.com>
2024-02-05 14:35:59 -0700

    added error handling

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pandas as pd
 from data_acquisition import get_listings
 from data_acquisition import organize_property_details
 from create_database import create_database
+from analysis import perform_analysis
+from prediction import predict
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
@@ -51,13 +53,13 @@ def create():
 
 
 @app.route('/predict', methods=('GET', 'POST'))
-def predict():
+def predictPage():
     if request.method == 'POST':
         try:
            predict() 
         except:
             flash("Error: please make sure that you have already created the database")
-            print("Error: Please check that the URL is valid and try again. If the problem persists, check if your API key has expired for the month.")
+            print("Error: please make sure that you have already created the database")
     return render_template('predict.html')
 
 if __name__ == "__main__":
