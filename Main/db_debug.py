6d776597b353d9e458631830ae05453a9c813ecd
BasedWRLD <kevin2845ca@gmail.com>
2024-02-13 13:22:57 -0700

    added favorite table to drop

--- Main/db_debug.py
+++ Main/db_debug.py
@@ -4,6 +4,7 @@ conn = sqlite3.connect('zillow_listings.db')
 c = conn.cursor()
 
 c.execute(' DROP TABLE IF EXISTS propertyDetails;')
+c.execute(' DROP TABLE IF EXISTS favoriteList')
 c.execute(' DROP TABLE IF EXISTS listings;')
 
 conn.commit()
