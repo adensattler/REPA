a5837ecef5acf4789910402d84491c4b6227172a
BasedWRLD <kevin2845ca@gmail.com>
2024-02-13 13:14:07 -0700

    added database drop file

---
+++ Main/db_debug.py
@@ -0,0 +1,10 @@
+import sqlite3 
+
+conn = sqlite3.connect('zillow_listings.db')
+c = conn.cursor()
+
+c.execute(' DROP TABLE IF EXISTS propertyDetails;')
+c.execute(' DROP TABLE IF EXISTS listings;')
+
+conn.commit()
+conn.close()
