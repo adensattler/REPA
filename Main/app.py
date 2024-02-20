4fca0bc4dc2232b4e8d211de2968fa3b348613f4
Aden Sattler <adensattler@gmail.com>
2024-02-20 14:54:54 -0700

    built route for listings search

--- Main/app.py
+++ Main/app.py
@@ -112,6 +112,59 @@ def create():
 # NEW ROUTES FOR VERSION 2 OF THE APP
 # --------------------------------------------------------------------------------------------------------
 
+@app.route('/listings-search', methods=('GET', 'POST'))
+def listings_search():
+    database = DatabaseManager('zillow_listings.db')
+    # if a post request is made lets check out what was posted!
+    if request.method == 'POST':
+        if "search" in request.form:
+                
+            # get the url from the form that was submitted
+            url = request.form['url']
+            try:
+                listing_url = url
+
+                listing_response = get_listings(API_KEY, listing_url)
+
+                # stores the columns we are interested in
+                columns = [
+                    'zpid', 'hdpData.homeInfo.price', 'hdpData.homeInfo.bedrooms', 'hdpData.homeInfo.bathrooms', 'area',
+                    'hdpData.homeInfo.zipcode', 'hdpData.homeInfo.livingArea', 'hdpData.homeInfo.homeType', 'hdpData.homeInfo.zestimate', 'hdpData.homeInfo.city', 'hdpData.homeInfo.latitude', 'hdpData.homeInfo.longitude',
+                    'hdpData.homeInfo.taxAssessedValue'
+                ]
+
+                # Takes all of the data and converts it into normalized, tabular data (.json_normalize)
+                den_listings = pd.json_normalize(listing_response["data"]["cat1"]["searchResults"]["mapResults"])
+                selected_den_listings = den_listings.loc[:, columns].dropna(thresh=13)
+                database.fill_database(selected_den_listings)
+                return render_template('listings_search.html', success_message = "Search was successful!")
+            except:
+                flash("Error: Please check that the URL is valid and try again. If the problem persists, check if your API key has expired for the month.")
+
+        elif "predict" in request.form:
+            try:
+                database_connection = sqlite3.connect('zillow_listings.db')
+                dataframe = pd.read_sql_query("SELECT * FROM listings", database_connection)
+                database_connection.close()
+
+                top_five_properties = perform_prediction_gui(dataframe)
+                return render_template('listings_search.html', predict_result=top_five_properties.to_html())
+            except:
+                flash("zillow_listings.db is empty. Please run --url first.")
+
+        elif "analyze" in request.form:
+            try:
+                database_connection = sqlite3.connect('zillow_listings.db')
+                dataframe = pd.read_sql_query("SELECT * FROM listings", database_connection)
+                database_connection.close()
+                
+                summary_table = create_summary_table(dataframe).to_html()    
+                return render_template('listings_search.html', analyze_result=summary_table)
+            except:
+                flash("zillow_listings.db is empty. Please run --url first.")
+            
+    return render_template('listings_search.html')
+
 # ROUTE TO THE PROPERTY SEARCH PAGE
 @app.route('/propertysearch', methods=('GET', 'POST'))
 def property_search():
