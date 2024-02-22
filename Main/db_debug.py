0cc539b26c00b4f39cea62f909e1ae91b8e9abfa
BasedWRLD <kevin2845ca@gmail.com>
2024-02-22 08:10:08 -0700

    fixed debug table drop order

--- Main/db_debug.py
+++ Main/db_debug.py
@@ -4,8 +4,8 @@ def resetDB():
     conn = sqlite3.connect('zillow_listings.db')
     c = conn.cursor()
     
-    c.execute(' DROP TABLE IF EXISTS propertyDetails;')
     c.execute(' DROP TABLE IF EXISTS favoriteList')
+    c.execute(' DROP TABLE IF EXISTS propertyDetails;')
     c.execute(' DROP TABLE IF EXISTS listings;')
     
     # c.execute(' VACUUM ')
