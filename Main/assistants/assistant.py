9c2d088a01ca9ed79f2db5f2cab33bb21ec8acd9
Aden Sattler <adensattler@gmail.com>
2024-03-01 13:02:40 -0700

    built all assistant helper functions. created assistant in driver

--- Main/assistants/assistant.py
+++ Main/assistants/assistant.py
@@ -15,6 +15,8 @@ Usage:
 
 from openai import OpenAI
 import os
+import shelve
+import time
 
 # Utilizes an environmental variable for your api key. will error out if not set in your system.
 client = OpenAI()
@@ -29,11 +31,108 @@ def upload_file(path):
     file = client.files.create(file=open(path, "rb"), purpose="assistants")
     return file
 
-def create_assistant():
+def create_assistant(file):
     assistant = client.beta.assistants.create(
-        name="Math Tutor",
-        instructions="You are a personal math tutor. Answer questions briefly, in a sentence or less.",
+        name="Real Estate Advisor",
+        instructions="""You are a highly knowledgeable real estate advisor. 
+        Your role is to summarize extensive property data, extract key figures and data, and give advice on a property.
+        Answer questions thoroughly.""",
+        tools=[{"type": "retrieval"}],
         model="gpt-4-1106-preview",
+        file_ids=[file.id],
     )
     return assistant
+
+# --------------------------------------------------------------
+# Thread Management Functions
+# --------------------------------------------------------------
+def check_if_thread_exists(zpid):
+    with shelve.open("threads_db") as threads_shelf:
+        return threads_shelf.get(zpid, None)
+
+def store_thread(zpid, thread_id):
+    with shelve.open("threads_db", writeback=True) as threads_shelf:
+        threads_shelf[zpid] = thread_id
+
+# DESC: These functions utilize the "shelve" library.
+# Think of a shelf as a dictionary that persists on a filesystem
+# We are using them to store our threads for each zpid!  
+# notice there is no create thread func because its super easy( see generate_response) 
+
+
+# --------------------------------------------------------------
+# Get a response to a message!
+# --------------------------------------------------------------
+def generate_response(message_body, zpid):
+    # Check if there is already a thread_id for the wa_id
+    thread_id = check_if_thread_exists(zpid)
+
+    # If there is no thread for a property, create one and store it
+    if thread_id is None:
+        print(f"Creating new thread for zpid {zpid}")
+        thread = client.beta.threads.create()
+        store_thread(zpid, thread.id)
+        thread_id = thread.id
+
+    # Otherwise, retrieve the existing thread from the shelf
+    else:
+        print(f"Retrieving existing thread for zpid {zpid}")
+        thread = client.beta.threads.retrieve(thread_id)
+
+    # Add message to thread
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=message_body,
+    )
+
+    # RUN THE ASSISTANT AND RETURN ITS RESPONSE
+    new_message = run_assistant(thread)
+    print(f"To User:", new_message)
+    return new_message
+
+
+# --------------------------------------------------------------
+# Run the assistant!
+# --------------------------------------------------------------
+def run_assistant(thread):
+    # Retrieve the Assistant
+    assistant = client.beta.assistants.retrieve(assistant_id)
+
+    # Run the assistant
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+    )
+
+    # Wait for completion
+    while run.status != "completed":
+        # Be nice to the API
+        time.sleep(0.5)
+        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+
+    # Retrieve the Messages
+    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    new_message = messages.data[0].content[0].text.value
+    print(f"Generated message: {new_message}")
+    return new_message
+
+
+# DRIVER
+# ------------------------------------------------------------------------------------------
+# STEP 1: Upload a file to OpenAI embeddings
+filepath = "prop-det-short.json"
+file_object = upload_file(filepath)
+
+
+# STEP 2: Create your assistant (Uncomment this once a single assistant has been created)
+# assistant = create_assistant(file_object)
+# assis_id = assistant.id
+# print(assis_id)
+
+# === Hardcoded assistant id (will be used after first run and the assistant is created) ===
+# we want ONE assistant with many different threads running off of it for specific applications!
+assistant_id = "asst_4R1G5OysL5oRCAwbiFEvxVYo"
+
+
 
