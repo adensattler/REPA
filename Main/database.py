b5313226888e4c3523e98ecf35e3a6a1e6149ded
Relja Eskic <relja.eskic@du.edu>
2024-03-12 21:59:24 -0600

    fixed carousel and property cards and working on other pages

--- Main/database.py
+++ Main/database.py
@@ -1,5 +1,4 @@
 import sqlite3
-import pandas as pd
 import json
 
 class DatabaseManager:
@@ -61,7 +60,6 @@ class DatabaseManager:
             num_beds INTEGER,
             num_baths INTEGER,
             zestimate INTEGER,
-            rentZestimate INTEGER,
             sqft INTEGER,
             price_per_sqft INTEGER,
             house_type TEXT,
@@ -71,6 +69,7 @@ class DatabaseManager:
             nearby_schools BLOB,
             nearby_cities BLOB,
             images TEXT,
+            thumbnail TEXT,
             description TEXT,
             monthly_rent_ REAL,
             cap_rate REAL,
@@ -98,6 +97,7 @@ class DatabaseManager:
         self.conn.commit()
         c.close()
 
+
     def fill_database(self, dataframe):
         with self.conn:
             c = self.conn.cursor()
@@ -108,6 +108,7 @@ class DatabaseManager:
                           (row['zpid'], row['hdpData.homeInfo.price'], row['hdpData.homeInfo.bedrooms'], row['hdpData.homeInfo.bathrooms'], row['area'], row['hdpData.homeInfo.zipcode'], row['hdpData.homeInfo.livingArea'], row['hdpData.homeInfo.homeType'], row['hdpData.homeInfo.zestimate'], row['hdpData.homeInfo.city'], row['hdpData.homeInfo.latitude'], row['hdpData.homeInfo.longitude'], row['hdpData.homeInfo.taxAssessedValue']))
         self.conn.commit()
         c.close()
+
 
     # This function given a zpid returns the raw json of a property
     # From the Database
@@ -122,7 +123,6 @@ class DatabaseManager:
         return dataRaw
 
 
-
     def insert_property_db(self,zpid,data):
         with self.conn:
             c = self.conn.cursor()
@@ -130,11 +130,8 @@ class DatabaseManager:
             inDB = c.execute(sql, (zpid,)).fetchone()[0]
             if (inDB < 1):
                 # Insert data into the table
-                c.execute('INSERT INTO propertyDetails (zillow_ID) VALUES (?)',(zpid ,))
-
-                #For future: update data with the full JSON 
-                # data = json.loads(self.get_JSON(zpid))
-                data=json.loads(data)
+                c.execute('INSERT INTO propertyDetails (zillow_ID,raw_json) VALUES (?,?)',(zpid , data))
+                data = json.loads(self.get_JSON(zpid))
                 
                 #Insert data for each column
                 self.update_property_db(zpid, "streetAddress" , data["data"]["address"]["streetAddress"])
@@ -152,27 +149,39 @@ class DatabaseManager:
                 self.update_property_db(zpid, "house_type", data["data"]["homeType"])
                 # self.add_nearby_homes(data["data"]["adTargets"]["mlat"], data["data"]["adTargets"]["mlong"])
 
-                self.update_property_db(zpid,"thirty_year_mortgage", data["data"]["mortgageRates"]["thirtyYearFixedRate"])
-                self.update_property_db(zpid,"fifteen_year_mortgage", data["data"]["mortgageRates"]["fifteenYearFixedRate"])
-                self.update_property_db(zpid,"rentZestimate", data["data"]["rentZestimate"])
-
-    
                 # TODO: Schools
                 # TODO: Nearby Cities
-    
-                # TODO: Get more Images
-                self.update_property_db(zpid, "images", data['data']['responsivePhotos'][0]['mixedSources']['jpeg'][7]['url'])
+            
+                # Loop through all images
+                img_urls = []
+                for img in data['data']['responsivePhotos']:
+                    img_urls.append(img['mixedSources']['jpeg'][7]['url'])
+                
+                # Serialize the data (list of URLs) to JSON before storing in the database
+                img_urls_json = json.dumps(img_urls)
+                self.update_property_db(zpid, "images", img_urls_json)
+
+                # Store 1st image as thumbnail for property_search page
+                self.update_property_db(zpid, "thumbnail", data['data']['responsivePhotos'][0]['mixedSources']['jpeg'][7]['url'])
+
                 self.update_property_db(zpid, "description", data["data"]["description"])
-                self.conn.commit()
+                self.conn.commit()    
             c.close()
+
 
     def update_property_db(self, zpid, field, data):
         with self.conn:
             c = self.conn.cursor()
-            sql = 'UPDATE propertyDetails SET ' + field + ' = "' + str(data) + '" WHERE zillow_ID = ' + zpid
-            c.execute(sql)
+            if field == 'images':
+                # Use parameterized queries to handle special characters
+                sql = 'UPDATE propertyDetails SET images = ? WHERE zillow_ID = ?'
+                c.execute(sql, (data, zpid))
+            else:
+                sql = 'UPDATE propertyDetails SET ' + field + ' = "' + str(data) + '" WHERE zillow_ID = ' + zpid
+                c.execute(sql)
         self.conn.commit()
         c.close()
+
 
     def get_value_from_property_db(self, zpid, key):
         with self.conn:
@@ -183,6 +192,16 @@ class DatabaseManager:
         self.conn.commit()
         c.close()
         return data[0]
+    
+
+    def get_images_from_property_db(self, zpid):
+        with self.conn:
+            c = self.conn.cursor()
+            self.conn.row_factory = sqlite3.Row
+            sql = "SELECT images FROM propertyDetails WHERE zillow_ID = ?"
+            data = c.execute(sql, (zpid,)).fetchone()
+        return data[0] if data else None
+
 
 # SOME HELPER FUNCTIONS
     def get_property_from_db(self, zpid):
@@ -190,8 +209,7 @@ class DatabaseManager:
         with self.conn:
             self.conn.row_factory = sqlite3.Row
             c = self.conn.cursor()
-            # property = c.execute('SELECT zillow_ID, streetAddress, price, num_beds, num_baths, zestimate, sqft, price_per_sqft, house_type, property_tax, nearby_schools, nearby_cities, images, description, images FROM propertyDetails WHERE zillow_ID = ?', (zpid,)).fetchone()
-            property = c.execute('SELECT * FROM propertyDetails WHERE zillow_ID = ?', (zpid,)).fetchone()
+            property = c.execute('SELECT zillow_ID, streetAddress, price, num_beds, num_baths, zestimate, sqft, price_per_sqft, house_type, property_tax, nearby_schools, nearby_cities, images, description, images FROM propertyDetails WHERE zillow_ID = ?', (zpid,)).fetchone()
         self.conn.commit()
         c.close()
         if property is not None:
@@ -199,9 +217,11 @@ class DatabaseManager:
         else:
             return None
 
+
     def get_prop_search_history(self):
         properties = self.sql_data_to_list_of_dicts("SELECT * FROM propertyDetails LIMIT 8")
         return properties
+
 
     def sql_data_to_list_of_dicts(self, select_query):
         # Returns data from an SQL query as a list of dicts.
@@ -213,6 +233,8 @@ class DatabaseManager:
         self.conn.commit()
         c.close()
         return unpacked
+
+
     def add_to_favorites(self, zpid):
         with self.conn:
             c = self.conn.cursor()
@@ -221,6 +243,7 @@ class DatabaseManager:
             if c.fetchone()[0] == 0:  # If not already in favorites
                 c.execute("INSERT INTO favoriteList (zillow_ID) VALUES (?)", (zpid,))
         self.conn.commit()
+
 
     def get_favorite_properties(self):
         with self.conn:
@@ -231,6 +254,7 @@ class DatabaseManager:
         self.conn.commit()
         return [dict(ix) for ix in favorites]
 
+
     def add_nearby_homes(self, zpid):
             lat = self.get_value_from_property_db(zpid, "latitude") 
             long = self.get_value_from_property_db(zpid, "longitude")
@@ -240,11 +264,11 @@ class DatabaseManager:
             bottom = float(lat) - 0.2
             left = float(long) - 0.2
             right = float(long) + 0.2
-        
             zillow_url = "https://www.zillow.com/homes/for_sale/?searchQueryState=%7B%22isMapVisible%22%3Atrue%2C%22mapBounds%22%3A%7B%22west%22%3A"+str(left)+"%2C%22east%22%3A"+str(right)+"%2C%22south%22%3A"+str(bottom)+"%2C%22north%22%3A"+str(top)+"%7D%2C%22filterState%22%3A%7B%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%7D%2C%22isListVisible%22%3Atrue%7D"
             self.conn.commit()
             return zillow_url
     
+
     def get_area_prices(self, zipcode):
         with self.conn:
             self.conn.row_factory = sqlite3.Row
@@ -252,3 +276,25 @@ class DatabaseManager:
             prices = c.execute("SELECT price FROM listings WHERE zipcode = (?)",(zipcode,)).fetchall()
             return [dict(ix) for ix in prices]
 
+
+    def remove_from_favorites(self, zipcode):
+        with self.conn as conn:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM favoriteList WHERE zillow_ID = ?", (zipcode,))
+            conn.commit()
+
+
+    def is_favorite(self, zpid):
+        """Check if a property is marked as a favorite.
+
+        Args:
+            zpid (str): The Zillow Property ID to check.
+
+        Returns:
+            bool: True if the property is a favorite, False otherwise.
+        """
+        with self.conn as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT COUNT(*) FROM favoriteList WHERE zillow_ID = ?", (zpid,))
+            count = cursor.fetchone()[0]
+            return count > 0
