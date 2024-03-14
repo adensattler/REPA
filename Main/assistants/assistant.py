dbe0e83c1d61b8d4b1015e43008feae0f0de0de7
Aden Sattler <adensattler@gmail.com>
2024-03-14 11:40:23 -0600

    commented assistant.py

--- Main/assistants/assistant.py
+++ Main/assistants/assistant.py
@@ -28,31 +28,20 @@ from data_acquisition import get_property_detail
 from database import DatabaseManager
 from config import API_KEY
 
+# === Hardcoded assistant id ===
+ASSISTANT_ID = "asst_jzMUaqyzjNcKF4oAKbzLYYjh"  # we want ONE assistant with many different threads running off of it for specific applications!
+
 client = OpenAI()
-# defaults to getting the key using os.environ.get("OPENAI_API_KEY") and will error out if not set in your system.
-# if you have the key set under a different name or not at all, you can pass the key as a parameter:
+# NOTE: OpenAI() defaults to getting your key using os.environ.get("OPENAI_API_KEY") and will error out if not set in your system.
+# if you have the key set under a different name or not at all, you can pass the key as a parameter instead:
 # client = OpenAI(api_key="YOUR_API_KEY_HERE")
 
 
 # HELPER FUNCTIONS
 # ---------------------------------------------------------------------
 
-# Upload file a that can be used across various endpoints. returns an OpenAI File object
-def upload_file(path):
-    file = client.files.create(file=open(path, "rb"), purpose="assistants")
-    return file
-
-# upload the json data instead of a file! THIS IS PROOF OF CONCEPT.
-def upload_json_test():
-    response = get_property_detail(API_KEY, 2054235341)
-    data = response.json()
-    print(type(data))
-
-    # Expected entry at `file` parameter to be bytes, an io.IOBase instance, PathLike or a tuple
-    file = client.files.create(file=json.dumps(data).encode(), purpose="assistants")
-    return file
-
-def upload_json(data):
+# Upload file-like object that can be used across various endpoints. returns an OpenAI File object.
+def upload_data(data):
     # Expected entry at `file` parameter to be bytes, an io.IOBase instance, PathLike or a tuple
     file = client.files.create(file=json.dumps(data).encode(), purpose="assistants")
     return file
@@ -92,26 +81,26 @@ def store_thread(zpid, thread_id):
 
 
 # --------------------------------------------------------------
-# Get a response to a message!
+# Get a response to a message about a property!
 # --------------------------------------------------------------
 def generate_response(message_body, zpid):
-    # Check if there is already a thread_id for the wa_id
+    # Check if there is already a thread_id for a zpid that is saved
     thread_id = check_if_thread_exists(zpid)
 
     # If there is no thread for a property, create one and store it
     if thread_id is None:
-        # Thread should be initialized with a message containing the property file
+        # A new thread should be initialized with a file containing all the data on a property
 
         # 1.) get the property json data from the database
         db = DatabaseManager('zillow_listings.db')
-        json_obj = json.loads(db.get_JSON(zpid))
-        print(type(json_obj)) # this type is a dict
+        data_obj = json.loads(db.get_JSON(zpid))
 
         # 2.) convert the json to an OpenAI file object
-        file = client.files.create(file=json.dumps(json_obj).encode(), purpose="assistants")
+        # file = client.files.create(file=json.dumps(json_obj).encode(), purpose="assistants")
+        file = upload_data(data_obj)
 
         # 3.) create the thread. pass the thread the file object!
-        # print(f"Creating new thread for zpid {zpid}")
+        print(f"Creating new thread for zpid {zpid}")
         thread = client.beta.threads.create(    
             messages=[
                 {
@@ -136,9 +125,9 @@ def generate_response(message_body, zpid):
         content=message_body,
     )
 
-    # RUN THE ASSISTANT AND RETURN ITS RESPONSE
+    # RUN THE THREAD AND RETURN ITS RESPONSE
     new_message = run_assistant(thread)
-    print(f"To User:", new_message)
+    print(f"To User:", new_message)     # logging the response for debugging
     return new_message
 
 # --------------------------------------------------------------
@@ -146,58 +135,59 @@ def generate_response(message_body, zpid):
 # --------------------------------------------------------------
 def run_assistant(thread):
     # Retrieve the Assistant
-    assistant = client.beta.assistants.retrieve(assistant_id)
+    assistant = client.beta.assistants.retrieve(ASSISTANT_ID)
 
-    # Run the assistant
+    # Run the provided thread
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
         assistant_id=assistant.id,
     )
 
-    # Wait for completion
+    # Wait for the API/LLM to finish running
     while run.status != "completed":
         # Be nice to the API
         time.sleep(0.5)
         run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
 
-    # Retrieve the Messages
+    # Retrieve the the messages in the thread and get the most recent response
     messages = client.beta.threads.messages.list(thread_id=thread.id)
     new_response = messages.data[0].content[0].text.value
 
-    # Use re.sub() to remove the matched source
-    # tags and their contents
+    # Use re.sub() to remove the matched source tags from the API response
     pattern = r'【\d+†source】'
     cleaned_response = re.sub(pattern, '', new_response)
 
     return cleaned_response
 
-# === Hardcoded assistant id (will be used after first run and the assistant is created) ===
-# we want ONE assistant with many different threads running off of it for specific applications!
-assistant_id = "asst_jzMUaqyzjNcKF4oAKbzLYYjh"
 
 def main():
-    # DRIVER
-    # ------------------------------------------------------------------------------------------
-    # # STEP 1: Upload a file to OpenAI embeddings
-    # filepath = os.path.join("property_details.json")
+    # This driver is used to test the AI Assistant from the command line.
+    
+    # INSTRUCTIONS:
+    # STEP 1: Pick a zpid of a property that is already in the database (specifically the propertyDetails table)
+    # STEP 2: Put that zpid in the second field of the generate_response function below.
+    # STEP 3: Run the program.
 
-    # file_object = upload_file(filepath)
-    # upload_json_test()
 
+    # NOTE: 
+    # The assistant id is HARDCODED. All threads are made from this single assistant.
+    # If you create a NEW assistant you must update the new assistant id in the ASSISTANT_ID variable.
+    # i.e. uncomment the code below, run it, then copy-paste the printed id into ASSISTANT_ID.
 
-    # STEP 2: Create your assistant (Uncomment this once a single assistant has been created)
+    # Create an assistant (Uncomment this to create a new assistant)
     # assistant = create_assistant()
     # assistant_id = assistant.id
     # print(assistant_id)
 
     
-
     while True:
         user_input = input("Please enter your message (or 'exit' to quit): ")
         if user_input.lower() == 'exit':
             break
         else:
-            generate_response(user_input, "13297098")  # Assuming a fixed zpid for simplicity
+            print()
+            generate_response(user_input, "247389523")
+                #         INSERT ZPID HERE ^^^^^^^^
 
 
 if __name__ == "__main__":
